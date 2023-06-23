90edc7059e9887dceda25e9eb3f99a64516dc8d2
The Test Coder <thetestcoder@gmail.com>
2023-06-23 16:55:27 +0530

    data provider added in test

--- tests/GstCalculatorTest.php
+++ tests/GstCalculatorTest.php
@@ -2,37 +2,31 @@
 
 namespace Tests;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Thetestcoder\GstCalculator;
 
 final class GstCalculatorTest extends TestCase
 {
-    public function test_it_calculate_gst()
+    #[DataProvider('gstCalculatorDataset')]
+    public function test_it_calculate_gst($amount, $percentage, $expected)
     {
         // arrange the data
-        $percentage = 18;
-        $amount = 100;
         $gstCalculator = new GstCalculator($percentage);
 
         // act on data
         $gst_amount = $gstCalculator->calculate($amount);
 
         // assert the response
-        $this->assertSame(18.0, $gst_amount);
+        $this->assertSame($expected, $gst_amount);
     }
 
-    public function test_it_calculate_gst_with_200_amount()
+    public static function gstCalculatorDataset()
     {
-        // arrange the data
-        $percentage = 18;
-        $amount = 200;
-        $gstCalculator = new GstCalculator($percentage);
-
-        // act on data
-        $gst_amount = $gstCalculator->calculate($amount);
-
-        // assert the response
-        $this->assertSame(36.0, $gst_amount);
+        yield [100, 18, 18.0];
+        yield [200, 18, 36.0];
+        yield [1000, 10, 100.0];
+        yield [50000, 18, 9000.0];
     }
 
     public function test_percentage_value_should_not_be_greater_than_100()
@@ -62,6 +56,5 @@ final class GstCalculatorTest extends TestCase
         // act on data
         $gst_amount = $gstCalculator->calculate($amount);
     }
-
 
 }
